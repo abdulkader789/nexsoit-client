051d51406c69d1dfaf6f85089a97895ac62ee545
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-05-01 00:37:54 +0600

    created responsive footer components and styled them

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Testimonial from '../components/Testimonial/Testimonial';
+import Footer from '../components/Footer/Footer';
 
 const HomePage = () => {
     return (
         <div>
             <Testimonial/>
+            <Footer/>
         </div>
     );
 };
