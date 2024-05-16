d8d9eb626e2b0233eb31221fd1911b3520d7db6a
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-05-16 22:54:40 +0600

    updated team component

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,9 +1,60 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShareNodes } from "@fortawesome/free-solid-svg-icons";
 
 const Team = () => {
+  const TeamData = [
+    {
+      image: "./team-01.jpg",
+      name: "Mike Hardson",
+      designation: "Director",
+    },
+    {
+      image: "./team-02.jpg",
+      name: "Aleesha Brown",
+      designation: "Director",
+    },
+    {
+      image: "./team-03.jpg",
+      name: "David Cooper",
+      designation: "Director",
+    },
+    {
+      image: "./team-04.jpg",
+      name: "David Cooper",
+      designation: "Director",
+    },
+  ];
   return (
-    <div>
-      <h1>This is Team Section</h1>
+    <div className="py-10">
+      <p className="uppercase text-center text-blue-700 text-sm">
+        professional team
+      </p>
+      <h2 className="text-center text-3xl font-bold text-gray-900">
+        Meet Our Experienced <br /> People Now
+      </h2>
+      <div className="max-w-6xl mx-auto my-8 px-2 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1">
+        {TeamData.map((item, index) => (
+          <div
+            key={index}
+            className="overflow-hidden max-w-[300px] mx-auto text-center lg:mb-0 md:md-4 mb-5"
+          >
+            <div className="relative">
+              <img
+                src={item.image}
+                alt="Blog Image"
+                className="object-cover w-full max-w-[250px] mx-auto rounded-full border-solid border-[15px] border-t-gray-200 border-r-gray-200 border-l-white border-b-white"
+              />
+              <FontAwesomeIcon
+                icon={faShareNodes}
+                className="bg-white text-gray-800 p-3 text-base rounded-full cursor-pointer shadow-md absolute lg:top-5 lg:left-[9%] md:top-8 md:left-[5%] top-5 left-[10%]"
+              />
+            </div>
+            <h3 className="text-xl font-bold mt-3">{item.name}</h3>
+            <p className="font-semibold text-gray-600">{item.designation}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutBanner from "../components/About/AboutBanner";
+import Team from "../components/Team/Team";
 
 const AboutPage = () => {
   return (
     <div>
       <AboutBanner />
+      <Team />
     </div>
   );
 };
