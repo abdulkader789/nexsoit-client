89afcd9a56eb1e03786b8aa6cea1dfe6a4ded7ac
Md. Abdul Kader <abdulemail789@gmail.com>
2024-06-04 03:16:52 +0600

    changed font family of our apporoch section

--- src/components/OurApproach/OurApproach.jsx
+++ src/components/OurApproach/OurApproach.jsx
@@ -34,12 +34,12 @@ const OurApproach = () => {
   return (
     <div>
       <section className="py-6">
-        <div className="text-center mb-8">
+        <div className="text-center poppins-regular  mb-8">
           <h4 className="text-lg text-gray-500">Our Approach</h4>
           <h2 className="text-3xl font-bold">Why Choose Us</h2>
         </div>
         <div className="w-2/3 mx-auto">
-          <p className="text-center text-gray-600">
+          <p className="text-center text-gray-600 poppins-regular">
             At OurCompany, we take pride in our customer-centric approach. Our
             team of experts is dedicated to understanding your unique needs and
             providing tailored solutions to help you achieve your goals. With a
@@ -49,24 +49,33 @@ const OurApproach = () => {
           </p>
         </div>
       </section>
-      <section className="w-full p-20 bg-orange-100">
+      <section className="w-full lg:px-40 px-2 lg:py-10 bg-orange-100">
         {cardData.map((card, index) => (
           <div
             key={index}
-            className={`w-full flex h-[500px] ${
-              index % 2 === 1 ? "flex-row-reverse" : "flex-row"
+            className={`w-full relative flex flex-col md:flex-row  mb-3 md:mb-0 md:h-[450px] ${
+              index % 2 === 1 ? "md:flex-row-reverse" : "md:flex-row"
             }`}
           >
-            <div className="h-full w-[45%] shadow-sm  bg-white flex flex-col justify-center items-center px-20">
-              <h3 className="text-4xl font-semibold mb-2">{card.title}</h3>
-              <p className="text-gray-600">{card.description}</p>
-            </div>
-            <div className="h-full w-[55%] border">
+            <div className="md:h-full md:w-[55%] h-60 border">
               <img
                 className="h-full w-full object-cover"
                 src={card.imageURL}
                 alt={card.title}
               />
+            </div>
+
+            <div className="h-full relative md:w-[45%] shadow-sm  bg-white flex flex-col justify-center items-center p-5 lg:px-20">
+              <h3 className="text-4xl font-semibold mb-2 montserrat-semibold">
+                {card.title}
+              </h3>
+              <p className="text-gray-600 work-sans">{card.description}</p>
+              <div
+                className={`absolute hidden md:flex top-[45%] h-20 w-20 text-2xl justify-center items-center text-white  rounded-full bg-black
+              ${index % 2 === 1 ? "-right-10" : "-left-10"}`}
+              >
+                {index + 1}
+              </div>
             </div>
           </div>
         ))}
