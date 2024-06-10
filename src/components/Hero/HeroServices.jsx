dcdd89e43bb8185e5a8241d063996a9d2cd9baea
Md. Abdul Kader <abdulemail789@gmail.com>
2024-06-10 16:21:48 +0600

    changed heroservice section. added bg images

--- src/components/Hero/HeroServices.jsx
+++ src/components/Hero/HeroServices.jsx
@@ -20,78 +20,96 @@ const colors = [
 
 const servicesData = [
   {
-    imageURL: <FaLaptopCode />,
+    icon: <FaLaptopCode />,
     heading: "Development",
     description:
       "Deliver robust software development services to build scalable and efficient solutions tailored to your business needs.",
     bgClass: colors[0],
     slug: "development",
+    image:
+      "https://media.licdn.com/dms/image/D4E12AQEnNKiiNUKz9Q/article-cover_image-shrink_720_1280/0/1693476739453?e=2147483647&v=beta&t=vA7q5Jo23C2UXQE4FQ3lTlpB7ugmZa5QZzAfSQ5rQ-A",
   },
   {
-    imageURL: <FaPencilRuler />,
+    icon: <FaPencilRuler />,
     heading: "Design",
     description:
       "Craft engaging and visually appealing designs that enhance user experience and reflect your brand's identity.",
     bgClass: colors[1],
     slug: "design",
+    image:
+      "https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww",
   },
   {
-    imageURL: <FaWrench />,
+    icon: <FaWrench />,
     heading: "Customization",
     description:
       "Provide customized solutions to modify existing systems or develop unique features that align with your specific requirements.",
     bgClass: colors[2],
     slug: "customization",
+    image:
+      "https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww",
   },
   {
-    imageURL: <FaVideo />,
+    icon: <FaVideo />,
     heading: "Editing",
     description:
       "Offer professional editing services for video content, ensuring high-quality and compelling visual narratives.",
     bgClass: colors[3],
     slug: "editing",
+    image:
+      "https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww",
   },
   {
-    imageURL: <FaBullhorn />,
+    icon: <FaBullhorn />,
     heading: "Marketing",
     description:
       "Implement strategic marketing solutions to enhance your online presence, drive traffic, and boost conversions.",
     bgClass: colors[4],
     slug: "marketing",
+    image:
+      "https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww",
   },
   {
-    imageURL: <FaTools />,
+    icon: <FaTools />,
     heading: "Support",
     description:
       "Provide comprehensive IT support, including maintenance, integration, and troubleshooting to ensure your systems run smoothly and efficiently.",
     bgClass: colors[5],
     slug: "support",
+    image:
+      "https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww",
   },
 ];
 
-const ServiceCard = ({ imageURL, heading, description, bgClass, slug }) => {
+const ServiceCard = ({ icon, heading, description, bgClass, slug, image }) => {
   const { handleCategoryClick } = useCategoryClick();
   return (
-    <div
-      className={`flex p-4 h-72 xl:mx-auto justify-center items-center border w-full bg-white text-black`}
-    >
-      <div className="flex flex-col lg:p-10">
-        {/* First Div - Icon */}
-        <div className={`text-5xl mb-3 flex justify-center text-blue-500`}>
-          {imageURL}
-        </div>
-        {/* Second Div - Header and Paragraph */}
-        <div className="flex flex-col justify-center">
-          <h2 className="mb-2 text-sm lg:text-xl text-center poppins-semibold">
-            {heading}
-          </h2>
-          <p className="text-sm work-sans text-center">{description}</p>
-          <div className="flex justify-center py-6">
-            <Link to={`/services`} onClick={() => handleCategoryClick(slug)}>
-              <button className="btn-learn-more z-10 py-2 text-sm px-5 poppins-regular">
-                Learn More
-              </button>
-            </Link>
+    <div className="relative overflow-hidden h-[70vh]">
+      <div
+        className={`flex flex-col justify-center items-center h-full w-full text-white relative overflow-hidden transform transition-transform duration-300 hover:scale-105 ${bgClass}`}
+      >
+        <div
+          className="absolute inset-0 bg-cover bg-center z-0"
+          style={{ backgroundImage: `url(${image})` }}
+        />
+        <div className="flex flex-col justify-center items-center z-10 p-4 bg-black bg-opacity-50 rounded-md">
+          {/* First Div - Icon */}
+          <div className="text-5xl mb-3 flex justify-center text-blue-500">
+            {icon}
+          </div>
+          {/* Second Div - Header and Paragraph */}
+          <div className="flex flex-col justify-center items-center text-center">
+            <h2 className="mb-2 text-sm lg:text-xl poppins-semibold">
+              {heading}
+            </h2>
+            <p className="text-sm work-sans">{description}</p>
+            <div className="flex justify-center py-6">
+              <Link to={`/services`} onClick={() => handleCategoryClick(slug)}>
+                <button className="btn-learn-more z-10 py-2 text-sm px-5 poppins-regular">
+                  Learn More
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
@@ -101,20 +119,21 @@ const ServiceCard = ({ imageURL, heading, description, bgClass, slug }) => {
 
 const HeroServices = () => {
   return (
-    <main className="relative z-30 lg:px-20 w-full">
-      <div className="h-full w-full relative lg:-top-40 shadow-md bg-gray-100 px-2 lg:px-20 py-10 rounded-md">
-        <h1 className="marcellus-regular uppercase text-2xl md:text-4xl mb-5 text-center">
+    <main className="relative w-full bg-slate-900">
+      <div className="h-full w-full relative shadow-md  rounded-md ">
+        {/* <h1 className="marcellus-regular uppercase text-2xl md:text-4xl mb-5 text-center">
           Empowering Digital Solutions
-        </h1>
-        <section className="grid grid-cols-1 sm:grid-cols-2 gap-3 w-full xl:grid-cols-3 xl:gap-5 py-5">
+        </h1> */}
+        <section className="grid grid-cols-1 h-full  w-full xl:grid-cols-3 px-32">
           {servicesData.map((item, index) => (
             <ServiceCard
               key={index}
-              imageURL={item.imageURL}
+              icon={item.icon}
               heading={item.heading}
               description={item.description}
               bgClass={item.bgClass}
               slug={item.slug}
+              image={item.image}
             />
           ))}
         </section>
--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -92,7 +92,7 @@ const AboutTeam = () => {
         </div>
         <div className="grid grid-cols-2 gap-4 mt-8">
           <img
-            className="w-full rounded-lg"
+            className="w-full rounded-lg "
             src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
             alt="office content 1"
           />
