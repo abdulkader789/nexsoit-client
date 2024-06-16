74fded18353642cb2c131389e5cac38b6fdd03f0
abdulkader789 <abdulemail789@gmail.com>
2024-06-16 19:52:18 +0600

    added svg in bannerslide

--- src/components/BannerSlider/BannerSlider.jsx
+++ src/components/BannerSlider/BannerSlider.jsx
@@ -113,7 +113,7 @@ const BannerSlider = () => {
                 autoplay={{ delay: 5000 }}
                 speed={2000}
                 modules={[ Autoplay, Navigation,  A11y ]}
-                className="mySwiper h-full w-full"
+                className="mySwiper h-full w-full relative"
                 simulateTouch={false}
               // navigation={{
               //   clickable: true,       
@@ -161,6 +161,13 @@ const BannerSlider = () => {
           );
         })}
       <SwiperNavButtons />
+      <div className='w-full absolute bottom-0 z-20 oveflow-hidden'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1250 250" >
+  <path fill="#ffffff" fill-opacity="1" d="M0,160L205.7,96L411.4,96L617.1,160L822.9,160L1028.6,192L1234.3,224L1440,288L1440,320L1234.3,320L1028.6,320L822.9,320L617.1,320L411.4,320L205.7,320L0,320Z"></path>
+  
+</svg>
+            </div>
+           
 
       </Swiper>
   
