7f2c255e4485f2b0310b6e4eb6e8e0fc2d058323
abdulkader789 <abdulemail789@gmail.com>
2024-06-17 00:26:32 +0600

    height increased of svg

--- src/components/Navbar/NavLogo.jsx
+++ src/components/Navbar/NavLogo.jsx
@@ -5,7 +5,7 @@ import navlogo from '../../../public/nexlogo.png'
 const NavLogo = () => {
   return (
     <div className="syne-bold ">
-      <Link className="h-full flex justify-center items-center" to="/">
+      <Link className="h-full flex justify-start items-center" to="/">
         {/* <p className="text-[12px]">Next Solution IT</p> */}
           <img className="h-6" src={navlogo}/>
                     <h1 className="text-sm text-black uppercase michroma-regular">Nexsoit</h1>
