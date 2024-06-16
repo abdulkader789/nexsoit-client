fd476a96cfc0042f04472c27d3c083e3a2e2243b
abdulkader789 <abdulemail789@gmail.com>
2024-06-16 17:12:03 +0600

    added svg inside div

--- src/feature/FeaturedHome.jsx
+++ src/feature/FeaturedHome.jsx
@@ -298,10 +298,12 @@ const FeaturedHome = ()=>{
             <div className='h-full  relative'>
             <BannerCarousel/>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1250 250" className='w-full absolute bottom-0 z-20'>
+            <div className='w-full absolute bottom-0 z-20 oveflow-hidden'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1250 250" >
   <path fill="#ffffff" fill-opacity="1" d="M0,160L205.7,96L411.4,96L617.1,160L822.9,160L1028.6,192L1234.3,224L1440,288L1440,320L1234.3,320L1028.6,320L822.9,320L617.1,320L411.4,320L205.7,320L0,320Z"></path>
   
 </svg>
+            </div>
            
         </div>
     )
