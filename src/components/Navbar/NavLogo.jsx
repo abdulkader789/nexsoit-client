3af1838d3bb66cf7653c367ab04c99318fd50190
abdulkader789 <abdulemail789@gmail.com>
2024-06-20 08:33:46 +0600

    changed logo font

--- src/components/Navbar/NavLogo.jsx
+++ src/components/Navbar/NavLogo.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
 import { useMobileNav } from "../../contexts/MobileNavContext";
 import LoaderShape from "../../utils/LoaderShape";
-import navlogo from '../../../public/nexlogo.png'
+import navlogo from "../../../public/nexlogo.png";
 const NavLogo = () => {
   return (
-    <div className="syne-bold ">
+    <div className="">
       <Link className="h-full flex justify-start items-center" to="/">
         {/* <p className="text-[12px]">Next Solution IT</p> */}
-          <img className="h-6" src={navlogo}/>
-                    <h1 className="text-sm text-black uppercase michroma-regular">Nexsoit</h1>
-
+        <img className="h-8" src={navlogo} />
+        <h1 className="text-2xl uppercase amita-bold">Nexsoit</h1>
       </Link>
     </div>
   );
