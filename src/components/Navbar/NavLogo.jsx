c51752bed126d765139ce6cd35153cc601e406f3
Md. Abdul Kader <abdulemail789@gmail.com>
2024-06-30 00:38:05 +0600

    added new logo

--- src/components/Navbar/NavLogo.jsx
+++ src/components/Navbar/NavLogo.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import { useMobileNav } from "../../contexts/MobileNavContext";
 import LoaderShape from "../../utils/LoaderShape";
-
+import agencyLogo from "../../../public/AgencyLogo.png";
 const NavLogo = () => {
   return (
     <div className="michroma-regular text-white ">
       <Link to="/">
         {/* <p className="text-[12px]">Next Solution IT</p> */}
         <div className="flex justify-center items-center ">
-          <h1 className="text-xl pl-5 font-extrabold uppercase">Nexsoit</h1>
+          {/* <h1 className="text-xl pl-5 font-extrabold uppercase">Nexsoit</h1> */}
+          <img src={agencyLogo} />
         </div>
       </Link>
     </div>
